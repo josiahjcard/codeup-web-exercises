745df4e2b113ca47fdaedef07b70ec2d073352a2
Josiah Cardenas <josiahjcard@gmail.com>
2023-11-16 19:51:26 -0600

    modified file

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,7 @@
 "use strict";
-let numInput = Number(prompt("Please enter an odd number between 1 and 50. "));
+let numInput;
 do {
+    numInput = Number(prompt("Please enter an odd number between 1 and 50. "));
     if (numInput % 2 === 0 || numInput < 1 || numInput > 50) {
         numInput = Number(prompt("Please re -enter an odd number between 1 and 50. "));
     } else {
@@ -18,29 +19,7 @@ for (let i = 0; i < 50; i++) {
     if (i % 2 !== 0) {
         console.log(`Here is an odd number: ${i}`);
     }
-
 }
-///////////////////////////////////////////////////////////////////////////////
-// let userNum = true;
-// while (userNum) {
-//     let userInput = Number(prompt("Please enter a number between 1 and 50. "));
-//     if(userInput > 1 || userInput < 50){
-//         break;
-//     } else {
-//         alert("Wrong, Please enter a number between 1 and 50. ");
-//     }
-//
-//     for(let i = 0; i < 50; i++) {
-//         if (userInput === i) {
-//             console.log(`Yikes! Skipping number: ${numInput}`);
-//             continue;
-//         }
-//         if (i % 2 !== 0) {
-//             console.log(`Here is an odd number: ${i}`);
-//         }
-//     }
-//
-// }
 
 
 
